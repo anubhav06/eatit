0d94847e734c8fb305332e9764ba72c5b949d3d7
Anubhav Gupta <mail.anubhav06@gmail.com>
2022-01-25 02:39:55 +0530

    added Restaurants AddFoodPage CSS

--- frontend/src/restaurants/pages/AddFoodItem.js
+++ frontend/src/restaurants/pages/AddFoodItem.js
@@ -1,7 +1,7 @@
 import React, {useContext} from 'react'
 import RestaurantAuthContext from '../context/RestaurantAuthContext'
 import RestaurantHeader from '../components/RestaurantHeader'
-
+import '../../components/LoginForm.css'
 
 const AddFoodItem = () => {
 
@@ -10,17 +10,30 @@ const AddFoodItem = () => {
     return (
         <div>
             <RestaurantHeader/>
-            You can add a new food item here:
-            <br/>
-            
-            <form onSubmit={addFoodItem}>
-                <input type="text" name="name" placeholder="Food name" required /> <br/>
-                <input type="text" name="description" placeholder="Food Description" required/> <br/>
-                <input type="number" min="0" step="0.01" name="price" placeholder="Price" required/> <br/>
-                <input type="file" accept="image/x-png,image/jpeg,image/jpg" name="image" placeholder="Food Image" required/> <br/>
-                
-                <input type="submit"/>
-            </form>
+
+            <div className='row'>
+                <div className='form-column-left'>
+                    <div className='form-background'>
+
+                        <div className='form-header'> Add food item here </div>
+                        <form onSubmit={addFoodItem}>
+                            <input type="text" name="name" placeholder="Food name" required className='form-input' /> <br/>
+                            <input type="text" name="description" placeholder="Food Description" required className='form-input'/> <br/>
+                            <input type="number" min="0" step="0.01" name="price" placeholder="Price" required className='form-input'/> <br/>
+                            <div className='form-file-input-label'> Upload the food's image: </div>
+                            <input type="file" accept="image/x-png,image/jpeg,image/jpg" name="image" placeholder="Food Image" required className='form-file-input'/> <br/>
+                            
+                            <input type="submit" className='form-submit-btn'/>
+                        </form>
+
+                    </div>
+                </div>
+                <div className='form-column-right'>
+                    <p className='formRight-heading'> EATIN </p>
+                    <p className='formRight-subHeading'> Add a new food item to your inventory </p>
+                    <p className='formRight-subHeading2'> Adding food made easy with EATIN </p>
+                </div>
+            </div>
         </div>
     )
 }
