75400c8753223c269799c354a722d4217ac3b7a5
Anubhav Gupta <mail.anubhav06@gmail.com>
2022-01-24 18:35:36 +0530

    added Restaurants Header CSS

--- frontend/src/restaurants/components/RestaurantHeader.js
+++ frontend/src/restaurants/components/RestaurantHeader.js
@@ -1,50 +1,79 @@
-import React, {useContext} from 'react'
-import { Link } from 'react-router-dom'
+import React, {useContext, useState, useEffect} from 'react'
 import RestaurantAuthContext from '../context/RestaurantAuthContext'
+import '../../components/Header.css'
+import logo from '../../assets/logo192.png'
+import { CSSTransition } from "react-transition-group";
 
 const RestaurantHeader = () => {
+
     // Get the variables and functions from context data in AuthContext
     let {restaurant, logoutRestaurant} = useContext(RestaurantAuthContext)
 
+    // For header navbar
+    const [isNavVisible, setNavVisibility] = useState(false);
+    const [isSmallScreen, setIsSmallScreen] = useState(false);
+  
 
+    useEffect(() => {
+        const mediaQuery = window.matchMedia("(max-width: 700px)");
+        mediaQuery.addEventListener([], handleMediaQueryChange(mediaQuery));
+        
     
+        return () => {
+          mediaQuery.removeEventListener([], handleMediaQueryChange(mediaQuery));
+        };
+    }, []);
+
+
+    const handleMediaQueryChange = mediaQuery => {
+        if (mediaQuery.matches) {
+          setIsSmallScreen(true);
+        } else {
+          setIsSmallScreen(false);
+        }
+    };
+
+    const toggleNav = () => {
+        setNavVisibility(!isNavVisible);
+    };
+
     return (
-        <div>
-            <Link to="/partner-with-us" >Home</Link>
+        <header className='Header'>
 
-            <span>  |  </span>
-            {/* If user is logged in then show logout button else show login button */}
-            {restaurant ? (
-                <Link to="/partner-with-us">
-                    <span onClick={logoutRestaurant}>Logout</span>
-                </Link>
-            ): (
-                <Link to="/partner-with-us/login" >Login</Link>
-            )}
+            <img src={logo} className='Logo' alt='logo' height='50'/>
+            
+            <CSSTransition
+                in={!isSmallScreen || isNavVisible}
+                timeout={350}
+                classNames="NavAnimation"
+                unmountOnExit
+            >
 
-            <span>  |  </span>
-            {restaurant ? (
-                <Link to="/partner-with-us/manage-food-items"> Manage Food Items </Link>
-            ) 
-            : (
-                <Link to="/partner-with-us/register" >Register</Link>
-            )}
+                {restaurant 
+                ?   <nav className='Nav'>
+                        {/* If user is logged in */}
+                        <a href="/restaurants" className='link'>Home</a>
+                        <a href='/my-account' className='link'> {restaurant.username} </a>
+                        <a href="/partner-with-us/manage-food-items" className='link'> Manage Food Items </a>
+                        <a href="/partner-with-us/add-food-item" className='link'> Add Food Item </a>
+                        <a href="/partner-with-us/account-setup" className='link'> Account Setup </a>
+                        <a href="" className='link' onClick={logoutRestaurant}> Logout </a>
+                    </nav>
+                :   <nav className='Nav'>
+                        {/* If user is logged out */}
+                        <a href ="/partner-with-us/login" className='link'> Login </a>
+                        <a href="/partner-with-us/register" className='link'> Register </a>
+                        <a href="/restaurants" className='link'> Main Site </a>
+                    </nav>
+                }
 
-            <span>  |  </span>
-            {restaurant ? (
-                <Link to="/partner-with-us/add-food-item"> Add Food Item </Link>
-            ) 
-            : (
-                <Link to="/restaurants" > Main Site </Link>
-            )}
+            </CSSTransition>
 
-            <span>  |   </span>
-            {restaurant ? (
-                <Link to="/partner-with-us/account-setup"> Account Setup </Link>
-            ) : (null)}
-                
+            <button onClick={toggleNav} className="Burger">
+                🍔
+            </button>   
            
-        </div>
+        </header>
     )
 }
 
