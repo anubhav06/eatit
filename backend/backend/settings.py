a6208cf5ade0b6a4571eac136cee64066d2ae368
Anubhav Gupta <mail.anubhav06@gmail.com>
2023-08-07 00:35:09 +0530

    add static files storage
    Signed-off-by: Anubhav Gupta <mail.anubhav06@gmail.com>

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -203,8 +203,8 @@ DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATICFILES_STORAGE="whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
