06eaa14dac3b652aff96c3734a33fccd9cf3b2da
Anubhav Gupta <mail.anubhav06@gmail.com>
2023-08-07 00:31:41 +0530

    add whitenoise to middlewares
    Signed-off-by: Anubhav Gupta <mail.anubhav06@gmail.com>

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -107,6 +107,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware'
 ]
 
 ROOT_URLCONF = 'backend.urls'
