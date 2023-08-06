251c1395e28e5bc71481c00da4a21b45c981a398
Anubhav Gupta <mail.anubhav06@gmail.com>
2023-08-07 01:51:42 +0530

    change db config
    Signed-off-by: Anubhav Gupta <mail.anubhav06@gmail.com>

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -137,22 +137,22 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'production': {
+    'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': config("PGDATABASE"),
         'USER': config("PGUSER"),
         'PASSWORD': config("PGPASSWORD"),
         'HOST': config("PGHOST"),
         'PORT': config("PGPORT"),
-    },
-    'dev': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    },
+    }
 }
 
-DATABASES['default'] = DATABASES['dev' if DEBUG else 'production']
-
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     },
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
